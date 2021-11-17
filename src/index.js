18a6502006f0b5edd9b2872a709eb329da59417c
Hamilkar247 <adonnibal96@gmail.com>
2021-11-17 10:47:35 +0100

    [WO-3] multipage - react - elektron
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,15 +4,47 @@ import './index.css';
 import App from './App';
 import Login from './Login'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Navigation,
+  Footer,
+  Home,
+  About,
+  Contact,
+  Blog,
+  Posts,
+  Post,
+} from "./components";
+
+//ReactDOM.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>,
+//  document.getElementById('root')
+//);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/blog" element={<Blog />}>
+        <Route path="" element={<Posts />} />
+        <Route path=":postSlug" element={<Post />} />
+      </Route>
+    </Routes>
+    <Footer />
+  </Router>,
+
+  document.getElementById("root")
 );
+
+////serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
