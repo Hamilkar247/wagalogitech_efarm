803542e0ca59d1420bbb4e74f0c18ef4c28bb8f5
Hamilkar247 <adonnibal96@gmail.com>
2021-12-14 15:40:05 +0100

    [WO-23] export csv - sesje uzytkownika
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- src/components/okna_sesji_uzytkownika/datatable_react_wagalogitech.jsx
+++ src/components/okna_sesji_uzytkownika/datatable_react_wagalogitech.jsx
@@ -59,6 +59,7 @@ const Table = props => {
         .indexOf(filterText.toLowerCase()) !== -1
   );
   
+
   const subHeaderComponent = useMemo(() => {
     const handleClear = () => {
       if (filterText) {
@@ -75,12 +76,63 @@ const Table = props => {
       />
     );
   }, [filterText, resetPaginationToggle]);
-  
+
+
+  function convertArrayOfObjectsToCSV(array) {
+    let result;
+
+    const columnDelimiter = ',';
+    const lineDelimiter = '\n';
+    const keys = Object.keys(props.data[0])
+
+    result = '';
+    result += keys.join(columnDelimiter)
+    
+    array.forEach(item => {
+          let ctr = 0;
+          keys.forEach(key =>{
+              if (ctr > 0) result += columnDelimiter;
+
+              result += item[key];
+
+              ctr++;
+          });
+          result += lineDelimiter;
+    });
+
+    return result;
+  }
+
+  function downloadCSV(array) {
+    const link = document.createElement('a');
+    let csv = convertArrayOfObjectsToCSV(array);
+    if (csv == null) return;
+
+    const filename = "export.csv";
+
+    if (!csv.match(/^data:text\/csv/i)) {
+      csv = `data:text/csv;charset=utf-8,${csv}`;
+    }
+
+    link.setAttribute('href', encodeURI(csv));
+    link.setAttribute('download', filename);
+    link.click();
+  }
+
+  const Export = ({ onExport }) => <button onClick ={e => onExport(e.target.value)}>Export</button>;
+
+  //const ExportCSV = () => {
+  const ExportCSV = React.useMemo(() => <Export onExport={() => downloadCSV(props.data)} />, []);
+  //}
+  //  = React.useMemo(() => <Export onExport={() => downloadCSV(data)} />, []);
+ 
+
   return (
     <DataTable
       title="Sesje Uzytkownika"
       columns={props.columns}
       data={filteredItems}
+      actions={ExportCSV}
       expandableRows
       defaultSortField="name"
       stripped
