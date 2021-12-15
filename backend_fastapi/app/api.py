9d72cf3a369f1e03783fe98d9f71a9d55016512b
Hamilkar247 <adonnibal96@gmail.com>
2021-12-15 17:10:29 +0100

    [WO-27] backend_fastapi
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

---
+++ backend_fastapi/app/api.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"]
+)
+
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to your todo list." }
+
+
+#uzytkownicy = [
+#    {
+#        id: 1,
+#        imie: "Hamilkar",
+#        nazwisko: "Barkas",
+#        login: "hami",
+#        haslo: "barkas",
+#        administrator: "true",
+#        ostatnio_zalogowany: "01.01.1970",
+#        konto_aktywne: "true"
+#    },
+#    {
+#        id: 2,
+#        imie: "Hannibal",
+#        nazwisko: "Barkas",
+#        login: "hanni",
+#        haslo: "barkas",
+#        administrator: "true",
+#        ostatnio_zalogowany: "01.01.1970",
+#        konto_aktywne: "true"
+#    },
+#    {
+#        id: 3,
+#        imie: "Hannon",
+#        nazwisko: "Eugoryda",
+#        login: "hanno",
+#        haslo: "barkas",
+#        administrator: "true",
+#        ostatnio_zalogowany: "01.01.1970",
+#        konto_aktywne: "true"
+#    },
+#    {
+#        id: 4,
+#        imie: "Magon",
+#        nazwisko: "Barkas",
+#        login: "magon",
+#        haslo: "barkas",
+#        administrator: "true",
+#        ostatnio_zalogowany: "01.01.1970",
+#        konto_aktywne: "true"
+#    },
+#    {
+#        id: 5,
+#        imie: "Hazdrubal",
+#        nazwisko: "Barkas",
+#        login: "hazdru",
+#        haslo: "barkas",
+#        administrator: "true",
+#        ostatnio_zalogowany: "01.01.1970",
+#        konto_aktywne: "true"
+#    }
+#];
