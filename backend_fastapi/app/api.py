2a298ddf1deaf12eec79969edc8d3487429b1a67
Hamilkar247 <adonnibal96@gmail.com>
2021-12-16 18:05:37 +0100

    [WO-28] api backend
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- backend_fastapi/app/api.py
+++ backend_fastapi/app/api.py
@@ -22,56 +22,76 @@ app.add_middleware(
 async def read_root() -> dict:
     return {"message": "Welcome to your todo list." }
 
+todos = [
+    {
+        "id": "1",
+        "item": "Read a book."
+    },
+    {
+        "id": "2",
+        "item": "Cycle around town."
+    }
+]
 
-#uzytkownicy = [
-#    {
-#        id: 1,
-#        imie: "Hamilkar",
-#        nazwisko: "Barkas",
-#        login: "hami",
-#        haslo: "barkas",
-#        administrator: "true",
-#        ostatnio_zalogowany: "01.01.1970",
-#        konto_aktywne: "true"
-#    },
-#    {
-#        id: 2,
-#        imie: "Hannibal",
-#        nazwisko: "Barkas",
-#        login: "hanni",
-#        haslo: "barkas",
-#        administrator: "true",
-#        ostatnio_zalogowany: "01.01.1970",
-#        konto_aktywne: "true"
-#    },
-#    {
-#        id: 3,
-#        imie: "Hannon",
-#        nazwisko: "Eugoryda",
-#        login: "hanno",
-#        haslo: "barkas",
-#        administrator: "true",
-#        ostatnio_zalogowany: "01.01.1970",
-#        konto_aktywne: "true"
-#    },
-#    {
-#        id: 4,
-#        imie: "Magon",
-#        nazwisko: "Barkas",
-#        login: "magon",
-#        haslo: "barkas",
-#        administrator: "true",
-#        ostatnio_zalogowany: "01.01.1970",
-#        konto_aktywne: "true"
-#    },
-#    {
-#        id: 5,
-#        imie: "Hazdrubal",
-#        nazwisko: "Barkas",
-#        login: "hazdru",
-#        haslo: "barkas",
-#        administrator: "true",
-#        ostatnio_zalogowany: "01.01.1970",
-#        konto_aktywne: "true"
-#    }
-#];
+@app.get("/todo", tags=["todos"])
+async def get_todos() -> dict:
+    return { "data": todos }
+
+
+uzytkownicy = [
+    {
+        "id": 1,
+        "imie": "Hamilkar",
+        "nazwisko": "Barkas",
+        "login": "hami",
+        "haslo": "barkas",
+        "administrator": "true",
+        "ostatnio_zalogowany": "01.01.1970",
+        "konto_aktywne": "true"
+    },
+    {
+        "id": 2,
+        "imie": "Hannibal",
+        "nazwisko": "Barkas",
+        "login": "hanni",
+        "haslo": "barkas",
+        "administrator": "true",
+        "ostatnio_zalogowany": "01.01.1970",
+        "konto_aktywne": "true"
+    },
+    {
+        "id": 3,
+        "imie": "Hannon",
+        "nazwisko": "Eugoryda",
+        "login": "hanno",
+        "haslo": "barkas",
+        "administrator": "true",
+        "ostatnio_zalogowany": "01.01.1970",
+        "konto_aktywne": "true"
+    },
+    {
+        "id": 4,
+        "imie": "Magon",
+        "nazwisko": "Barkas",
+        "login": "magon",
+        "haslo": "barkas",
+        "administrator": "true",
+        "ostatnio_zalogowany": "01.01.1970",
+        "konto_aktywne": "true"
+    },
+    {
+        "id": 5,
+        "imie": "Hazdrubal",
+        "nazwisko": "Barkas",
+        "login": "hazdru",
+        "haslo": "barkas",
+        "administrator": "true",
+        "ostatnio_zalogowany": "01.01.1970",
+        "konto_aktywne": "true"
+    }
+]
+
+@app.get("/uzytkownicy", tags=["uzytkownicy"])
+async def get_uzytkownicy() -> dict:
+    return { "data": uzytkownicy }
+
