4e5610a004c9174a94d6acd01597ffe7e42c22c9
Hamilkar247 <adonnibal96@gmail.com>
2022-01-11 16:15:44 +0100

    dzialajace_dane
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- src/components/nowa_seria_pomiarowa/TrwanieSeriiPomiarowej.jsx
+++ src/components/nowa_seria_pomiarowa/TrwanieSeriiPomiarowej.jsx
@@ -20,129 +20,8 @@ ColumnDirective
 } 
 from 
 '@syncfusion/ej2-react-spreadsheet'; 
-import { freezePaneData } from "./data";
+import { freezePaneData } from "./data_pomiary";
 
-// function getNumberOfPages(rowCount, rowsPerPage) {
-//     return Math.ceil(rowCount / rowsPerPage);
-//   }
-  
-// function toPages(pages) {
-//   const results = [];
-
-//   for (let i = 1; i < pages; i++) {
-//     results.push(i);
-//   }
-
-//   return results;
-// }
-  
-// const list_of_columns = [
-//   {
-//     name: "Wartość",
-//     selector: (row) => row.wartosc,
-//     sortable: true
-//   },
-//   {
-//     name: "Etykieta",
-//     selector: (row) => row.etykieta,
-//       sortable: true
-//   },
-//   {
-//     name: "Data",
-//     selector: (row) => row.data,
-//     sortable: true
-//   },
-//   {
-//     name: "Ważny",
-//     selector: (row) => row.wazny,
-//       sortable: true
-//     }
-// ];
-
-// // RDT exposes the following internal pagination properties
-// const BootyPagination = ({
-//   rowsPerPage,
-//   rowCount,
-//   onChangePage,
-//   onChangeRowsPerPage, // available but not used here
-//   currentPage
-//   }) => {
-//   const handleBackButtonClick = () => {
-//     onChangePage(currentPage - 1);
-//   };
-
-//   const handleNextButtonClick = () => {
-//     onChangePage(currentPage + 1);
-//   };
-
-//   const handlePageNumber = (e) => {
-//     onChangePage(Number(e.target.value));
-//   };
-
-//   const pages = getNumberOfPages(rowCount, rowsPerPage);
-//   const pageItems = toPages(pages);
-//   const nextDisabled = currentPage === pageItems.length;
-//   const previosDisabled = currentPage === 1;
-
-//   return (
-//       <nav>
-//         <ul className="pagination">
-//           <li className="page-item">
-//             <button
-//               className="page-link"
-//               onClick={handleBackButtonClick}
-//               disabled={previosDisabled}
-//               aria-disabled={previosDisabled}
-//               aria-label="previous page"
-//             >
-//               Poprzedni
-//             </button>
-//           </li>
-//           {pageItems.map((page) => {
-//             const className =
-//               page === currentPage ? "page-item active" : "page-item";
-  
-//             return (
-//               <li key={page} className={className}>
-//                 <button
-//                   className="page-link"
-//                   onClick={handlePageNumber}
-//                   value={page}
-//                 >
-//                   {page}
-//                 </button>
-//               </li>
-//           );
-//         })}
-//         <li className="page-item">
-//           <button
-//             className="page-link"
-//             onClick={handleNextButtonClick}
-//             disabled={nextDisabled}
-//             aria-disabled={nextDisabled}
-//           aria-label="next page"
-//           >
-//             Następny
-//           </button>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// const BootyCheckBox = React.forwardRef(({ onClick, ...rest }, ref) => (
-//     <div className="form-check">
-//       <input
-//         htmlFor="booty-check"
-//         type="checkbox"
-//         className="form-check-input"
-//         ref={ref}
-//      onClick={onClick}
-//       {...rest}
-//     />
-//     <label className="form-check-label" id="booty-check" />
-//   </div>
-// ));
 
 function List_of_buttons() {
     return (
@@ -240,9 +119,9 @@ function TrwanieSeriiPomiarowej() {
                     selectedRange="C1"
                   >
                     <RangesDirective>
-                      <RangeDirective dataSource={freezePaneData} startCell="A2" />              
+                      <RangeDirective dataSource={freezePaneData} startCell="C2" />              
                     </RangesDirective>
-                    <RowsDirective>
+                    {/* <RowsDirective>
                       <RowDirective>
                         <CellsDirective>
                           <CellDirective
@@ -268,7 +147,7 @@ function TrwanieSeriiPomiarowej() {
                           <CellDirective formular="=SUM(P4:P26)" style={cellStyle} />
                         </CellsDirective>
                       </RowDirective>
-                    </RowsDirective>
+                    </RowsDirective> */}
                     <ColumnsDirective>
                       <ColumnDirective width={80} />
                       <ColumnDirective width={80} />
