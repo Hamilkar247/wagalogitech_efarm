5b9acee39a04a35a25ff2c97bf7f959231ca76b8
Hamilkar247 <adonnibal96@gmail.com>
2022-01-11 17:54:29 +0100

    [#2] instalacja mongodb - motor i pymongo
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- backend_fastapi/main.py
+++ backend_fastapi/main.py
@@ -2,4 +2,13 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "app.api:app", 
+        host="0.0.0.0", 
+        port=8000, 
+        reload=True
+    )
+
+@app.on_event("startup")
+async def startup_db_client():
+    
