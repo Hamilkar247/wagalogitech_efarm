2de779f1d8725755eaac5d68bb3d03e87f6daa6d
Hamilkar247 <adonnibal96@gmail.com>
2022-01-12 10:52:14 +0100

    [#2] poczatek fastapi
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- backend_fastapi/main.py
+++ backend_fastapi/main.py
@@ -3,12 +3,13 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.api:app", 
+        "server.app:app", 
         host="0.0.0.0", 
         port=8000, 
         reload=True
     )
 
-@app.on_event("startup")
-async def startup_db_client():
+
+# @app.on_event("startup")
+# async def startup_db_client():
     
